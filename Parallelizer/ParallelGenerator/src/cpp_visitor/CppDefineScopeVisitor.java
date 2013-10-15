cf63a12b7b098e4a59dcf8ae2dc38a780f35f4f9
t1t0 <t1t0@tiscali.it>
2013-10-15 11:57:27 -0300

    attempt to define scope as a ClassParallelizer/ParallelGenerator/src/cpp_visitor/CppDefineScopeVisitor.java

---
+++ Parallelizer/ParallelGenerator/src/cpp_visitor/CppDefineScopeVisitor.java
@@ -0,0 +1,20 @@
+package cpp_visitor;
+
+import java.util.List;
+
+import model.statement.Statement;
+import visitor.DefineScopeVisitor;
+
+public class CppDefineScopeVisitor implements DefineScopeVisitor {
+	public void execute(String scopeName, List<Statement> statements) {
+		System.out.println("struct " + scopeName);
+		System.out.println("{");
+
+		for (Statement s : statements) {
+			System.out.print("    ");
+			s.execute();
+		}
+
+		System.out.println("};");
+	}
+}
